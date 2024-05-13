1aed1d03be6d20e7fa54016bd6d19f34b6661b6c
Abdollatif <a.latifnajafizadeh@gmail.com>
2024-05-13 19:13:35 +0330

    :/

--- ws/services/IO/IO.go
+++ ws/services/IO/IO.go
@@ -10,12 +10,3 @@ func BulkWriteInMemoryRoutes(incommingRoutes *gin.Engine) {
 	incommingRoutes.POST("/memory/write", memory.GetRequestForWriteOnMemory())
 	incommingRoutes.GET("/memory/:address", memory.ReadFromMemory())
 }
-
-// type BulkWriteMemoryRequest struct {
-// 	Address string `json:"address"`
-// 	Value   string `json:"value"`
-// }
-
-// // var dataBus *bus.DataBus
-// var globalDataBus *bus.DataBus
-// var globalMemory *memory.Memory
