8597d331211148fdeb3380ff2d4045d46d7e314f
Abdollatif <a.latifnajafizadeh@gmail.com>
2024-05-17 15:36:12 +0330

    implement bus

--- ws/services/bus/DataBus.go
+++ ws/services/bus/DataBus.go
@@ -1,23 +1,62 @@
 package bus
 
+import (
+	"architecture/ws/services/memory"
+	"errors"
+	"fmt"
+)
+
 type DataBus struct {
-	Data map[int][]byte
+	Address int
+	Data    int
+	Read    bool
+	Write   bool
 }
 
 func NewDataBus() *DataBus {
-	return &DataBus{
-		Data: make(map[int][]byte),
+	return &DataBus{}
+}
+
+func (bus *DataBus) SetAddress(address int) {
+	bus.Address = address
+}
+
+func (bus *DataBus) SetData(data int) {
+	bus.Data = data
+}
+
+func (bus *DataBus) EnableRead() {
+	bus.Read = true
+	bus.Write = false
+}
+
+func (bus *DataBus) EnableWrite() {
+	bus.Write = true
+	bus.Read = false
+}
+
+func (bus *DataBus) DisableSignals() {
+	bus.Read = false
+	bus.Write = false
+}
+
+func (bus *DataBus) PerformOperation(memory *memory.Memory, registers *memory.Registers) error {
+	if bus.Read {
+		data, err := memory.Read(bus.Address)
+		if err != nil {
+			return err
+		}
+		bus.Data = data
+	} else if bus.Write {
+		err := memory.Write(bus.Address, bus.Data)
+		if err != nil {
+			return err
+		}
+	} else {
+		return errors.New("no operation specified on the data bus")
 	}
+	return nil
 }
-
-func (d *DataBus) Read(address int) []byte {
-	return d.Data[address]
-}
-
-func (d *DataBus) Write(address int, data []byte) {
-	d.Data[address] = data
-}
-
-func (d *DataBus) Delete(address int) {
-	delete(d.Data, address)
+func (bus *DataBus) Dump() {
+	fmt.Printf("Address: %04X, Data: %04X, Read: %t, Write: %t\n", bus.Address, bus.Data, bus.Read, bus.Write)
 }
