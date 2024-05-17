24e075a8b46e0ffcac9e48cff88e1f44f97e934c
Abdollatif <a.latifnajafizadeh@gmail.com>
2024-05-17 11:39:32 +0330

    complete read and write in memory

---
+++ ws/services/IO/memoryController.go
@@ -0,0 +1,6 @@
+package io
+
+// func BulkWriteInMemoryRoutes(incommingRoutes *gin.Engine) {
+// 	incommingRoutes.POST("/memory/write", memory.GetRequestForWriteOnMemory())
+// 	incommingRoutes.GET("/memory/:address", memory.ReadFromMemory())
+// }
