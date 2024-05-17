315fe76eb74f36c984c2a6b74143ce4bc6383db9
Abdollatif <a.latifnajafizadeh@gmail.com>
2024-05-17 14:11:36 +0330

    implement cu

--- ws/services/controlunit/ControlUnit.go
+++ ws/services/controlunit/ControlUnit.go
@@ -1,5 +1,80 @@
 package controlunit
 
+import (
+	"architecture/ws/services/memory"
+	"errors"
+	"fmt"
+)
+
+type ControlUnit struct {
+	Register *memory.Registers
+	Memory   *Memory
+}
+type Memory struct {
+	Data map[int]int
+}
+
+func NewMemory() *Memory {
+	return &Memory{
+		Data: make(map[int]int),
+	}
+}
+
+func NewControlUnit(register *memory.Registers, memory *Memory) *ControlUnit {
+	return &ControlUnit{
+		Register: register,
+		Memory:   memory,
+	}
+}
+
+func (cu *ControlUnit) Fetch() {
+	cu.Register.IR = cu.Memory.Data[cu.Register.PC]
+	cu.Register.PC++
+}
+
+func (cu *ControlUnit) Decode() (string, int, error) {
+	opcode := (cu.Register.IR & 0xF000) >> 12
+	address := cu.Register.IR & 0x0FFF
+	return fmt.Sprintf("%X", opcode), address, nil
+}
+
+func (cu *ControlUnit) Execute(opcode string, address int) error {
+	switch opcode {
+	case "1":
+		cu.Register.DR = cu.Memory.Data[address]
+		cu.Register.AC += cu.Register.DR
+	case "2":
+		cu.Register.DR = cu.Memory.Data[address]
+		cu.Register.AC -= cu.Register.DR
+	case "3":
+		cu.Register.AC = cu.Memory.Data[address]
+	case "4":
+		cu.Memory.Data[address] = cu.Register.AC
+	case "5":
+		cu.Register.PC = address
+	case "6":
+		cu.Memory.Data[address] = cu.Register.PC
+		cu.Register.PC = address + 1
+	case "7":
+		cu.Memory.Data[address]++
+		if cu.Memory.Data[address] == 0 {
+			cu.Register.PC++
+		}
+	default:
+		return errors.New("unsupported opcode: " + opcode)
+	}
+	return nil
+}
+
+func (cu *ControlUnit) RunCycle() error {
+	cu.Fetch()
+	opcode, address, err := cu.Decode()
+	if err != nil {
+		return err
+	}
+	return cu.Execute(opcode, address)
+}
+
 // import (
 // 	"architecture/ws/services/bus"
 // 	"architecture/ws/services/memory"
